d093bbbd225f615d133f991802a4202fb4642319
Paylet Sen <sanchitsen55@gmail.com>
2025-06-07 10:39:26 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true, // Expose to local network (e.g. 192.168.x.x)
+    host: '0.0.0.0', // Expose to local network (e.g. 192.168.x.x)
     port: 5173, // Optional: default is 5173 anyway
   },
 })
